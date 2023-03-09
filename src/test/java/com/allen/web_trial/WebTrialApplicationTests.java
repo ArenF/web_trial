738405ad98c685fc2a405382f68eb4ff41c0027b
Allen <hjh040@gmail.com>
2023-03-10 00:14:45 +0900

    Initial commit

---
+++ src/test/java/com/allen/web_trial/WebTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allen.web_trial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

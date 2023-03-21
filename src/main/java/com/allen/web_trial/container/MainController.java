f229bddb8727277f5e5a8d6d847be44e9d90bbb8
Allen <hjh040@gmail.com>
2023-03-21 23:43:27 +0900

    create Test Case and Member Class

---
+++ src/main/java/com/allen/web_trial/container/MainController.java
@@ -0,0 +1,17 @@
+package com.allen.web_trial.container;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/controller")
+    public String controller(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "controller";
+    }
+}
---
+++ src/test/java/com/allen/web_trial/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.allen.web_trial.repository;
+
+import com.allen.web_trial.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        memoryMemberRepository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        memoryMemberRepository.save(member);
+
+        Member result = memoryMemberRepository.findById(member.getId()).get();
+
+        Assertions.assertEquals(member, result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        memoryMemberRepository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memoryMemberRepository.save(member2);
+
+        Member result = memoryMemberRepository.findByName("spring2").get();
+
+        Assertions.assertEquals(member2, result);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        memoryMemberRepository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memoryMemberRepository.save(member2);
+
+        List<Member> result = memoryMemberRepository.findAll();
+
+        Assertions.assertEquals(2, result.size());
+    }
+
+
+}

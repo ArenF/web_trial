93cb9aca7108aa38a0de5f22412adc59504b758e
Allen <hjh040@gmail.com>
2023-05-30 23:04:53 +0900

    change board table to board card form

--- src/main/frontend/src/Pages/Board.jsx
+++ src/main/frontend/src/Pages/Board.jsx
@@ -2,58 +2,50 @@ import React from "react";
 
 import "../CSS/BoardPage/board.css";
 
-class BoardList extends React.Component {
+//게시글 카드
+class CardBoard extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            image: this.props.imageSrc,
+            title: this.props.title,
+            contents: this.props.contents,
+            joinCount: this.props.join,
+            authorImage : this.props.authorSrc,
+        }
+    }
+
+    getStyleMessage() {
+        return {
+            background: 'url(' + this.state.image + ')',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: 'beige'
+        }
     }
 
     render() {
         return (
-            <div>
-                <table className="border">
-                    <thead>
-                        <tr>
-                            <td>번호</td>
-                            <td>제목</td>
-                            <td>글쓴이</td>
-                            <td>작성일</td>
-                            <td>조회수</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Test Title</td>
-                            <td>Author: AREN</td>
-                            <td>Date: 2023-23-23</td>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Test Title</td>
-                            <td>Author: AREN</td>
-                            <td>Date: 2023-12-12</td>
-                            <td>8</td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div className="cardBoard">
+                <div className="board-image" style={this.getStyleMessage()}></div>
+                <p className="board-title">{this.state.title}</p>
+                <p className="board-content">{this.state.contents}</p>
+                <div className="board-line"></div>
+                <div className="block">
+                    <div className="author-profile"></div>
+                    <div className="like">{this.state.joinCount}</div>
+                </div>
             </div>
         );
     }
 }
 
-function MainBoard(props) {
-    return(
-        <div className="main-card">
-            {props.children}
-        </div>
-    );
-}
-
 export default function App() {
     return(
-        <MainBoard>
-            <BoardList />
-        </MainBoard>
+        <div className="mainboard">
+            <CardBoard imageSrc="/resources/citystreet.jpg" title="CityStyle"
+            contents="we must face our city and society, city is beautiful but, also criminal and
+            lonely." />
+        </div>
     );
 }
